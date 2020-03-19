a4cfe5886738ba483251f56023be55e1183f0ac5
JaeHeong <rlawogud970301@gmail.com>
2020-03-19 17:03:29 +0900

    Updates

---
+++ precache-manifest.c61ea33dfe9a6d3b4c811522df256902.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f612d72634eddee9d61ca35eb3b6cd",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "4f240d5e76672ef0cec3",
+    "url": "/movie_app_2020/static/css/main.6bb9672a.chunk.css"
+  },
+  {
+    "revision": "ee5f40fffa95eb75bff2",
+    "url": "/movie_app_2020/static/js/2.41708d2b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.41708d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f240d5e76672ef0cec3",
+    "url": "/movie_app_2020/static/js/main.25bf922f.chunk.js"
+  },
+  {
+    "revision": "5b9de689d76c950937df",
+    "url": "/movie_app_2020/static/js/runtime-main.db2dcdc9.js"
+  }
+]);
